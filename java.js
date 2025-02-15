72af0016f0d3a9bda6251f3bfa0cc8db4401c2e2
09-Hala <halaashraf281@gmail.com>
2025-02-15 10:30:45 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+alert("hello");
+document.write("<p>hi world</p>");
+console.log(typeof 7);
+console.log(typeof "hala");
+console.log(typeof true);
+console.log(typeof "10");
+console.log(typeof halool);
+let x=10;
+console.log(x*2)
+console.log("new \n trying \n school")
+var myName= "hala";
+console.log("Hello" +  myName)
+console.log(`Hello ${myName}`)
